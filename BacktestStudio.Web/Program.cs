eb308cf1da02fbbfebd97d6fd38b886cccd0516f
Woody.Chiu <simoncat524@hotmail.com>
2025-08-11 23:48:17 +0800

    feat:調整專案路徑

---
+++ BacktestStudio.Web/Program.cs
@@ -0,0 +1,38 @@
+using BacktestStudio.Web.Components;
+using BacktestStudio.Web.Services;
+using ApexCharts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Register our services
+builder.Services.AddSingleton<AppState>();
+builder.Services.AddScoped<IApiService, MockApiService>();
+builder.Services.AddScoped<IChartService, ChartService>();
+builder.Services.AddHttpClient();
+
+// Register ApexCharts
+builder.Services.AddApexCharts();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
