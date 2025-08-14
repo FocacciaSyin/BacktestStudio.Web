f93ca66e9270c1a3512fac2085bcaddbc2fe468d
Woody.Chiu <simoncat524@hotmail.com>
2025-08-14 15:19:48 +0800

    feat: 完成欄位新增

--- BacktestStudio.Repository/Migrations/20250814070703_AddExtendedFields.Designer.cs
+++ BacktestStudio.Repository/Migrations/20250814070703_AddExtendedFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BacktestStudio.Repository.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BacktestStudio.Repository.Migrations
 {
     [DbContext(typeof(BackestStudioContext))]
-    partial class BackestStudioContextModelSnapshot : ModelSnapshot
+    [Migration("20250814070703_AddExtendedFields")]
+    partial class AddExtendedFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
---
+++ BacktestStudio.Repository/Migrations/20250814070703_AddExtendedFields.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BacktestStudio.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExtendedFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ProfitAmount",
+                table: "PurchaseRecords",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SettlementDate",
+                table: "PurchaseRecords",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "StopLossPrice",
+                table: "PurchaseRecords",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfitAmount",
+                table: "PurchaseRecords");
+
+            migrationBuilder.DropColumn(
+                name: "SettlementDate",
+                table: "PurchaseRecords");
+
+            migrationBuilder.DropColumn(
+                name: "StopLossPrice",
+                table: "PurchaseRecords");
+        }
+    }
+}
